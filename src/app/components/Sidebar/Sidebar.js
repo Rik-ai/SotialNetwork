ab96482c98a45026790a577e157cf3de907cf017
Ivan <ivan.lakhanin@gmail.com>
2020-04-12 22:33:19 +0300

    Some css styling

--- src/app/components/Sidebar/Sidebar.js
+++ src/app/components/Sidebar/Sidebar.js
@@ -4,22 +4,27 @@ import styled from '@emotion/styled'
 
 
 const SmallComp = styled.div`
-  width: 80%;
-  height: 40px;
-  background: green;
-  margin: 5px;
+  width: 70%;
+  height: 35px;
+  color: white;
+  opacity: 0.5;
+  padding-top: 10px;
+  margin: 20px auto;
+  border: 2px solid white;
+  border-radius: 10px;
   display: ${props => props.display};
+  text-align:center;
   ` 
 
 const Sidebar1 = styled.nav`
   position: relative;
-  height: calc(100vh - 75px);
-  overflow: auto;
-  width: 260px;
-  z-index: 4;
-  padding-bottom: 30px;
+  height: 100%;
+  border: 4px groove black;
+  width: 250px;
+  top: 0;
+  left: 0;
   opacity: 1;
-  background-image: url(${'https://s00.yaplakal.com/pics/pics_original/5/7/8/13224875.jpg'}); 
+  background-image: url(${'https://cdn.lifehacker.ru/wp-content/uploads/2019/09/ZB7FrVz_1569238415-310x672.jpg'}); 
   color: ${props=>props.color};
   ` 
 
@@ -37,7 +42,7 @@ const Sidebar =()=>{
             <SmallComp 
               key = {index} 
               display = {visible ? 'block' : 'none'}
-            > {item}
+            > {item} Hello matherfuckers !
             </SmallComp>
           )
         })
