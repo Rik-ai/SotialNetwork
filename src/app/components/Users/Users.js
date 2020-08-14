375f89600d717ad5d66ccd405ce9d7bdcb31cb58
Ivan <ivan.lakhanin@gmail.com>
2020-08-14 13:38:51 +0300

    Users API

--- src/app/components/Users/Users.js
+++ src/app/components/Users/Users.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import * as axios from 'axios'
+import userPhoto from '../../assets/images/images.png'
 
 const Div = styled.div`
 background-color:#242526;
@@ -18,9 +20,11 @@ overflow-y: auto;
 const Img = styled.img`
 width:100px;
 height:100px;
+border-radius: 20px;
 `
 const Button = styled.button`
 padding-left:5px;
+margin-bottom: 10px;
 display: inline-block;
 font-family:inherit;
 font-size:20px;
@@ -29,18 +33,10 @@ font-size:20px;
 const Users =(props)=>{
 
   if (props.users.length ===0){
-    props.setUsers([
-      {id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVqhTJArI2RLSSzlY0X1twqrkjMrScCsRWwQ&usqp=CAU',
-        followed: false, fullName: 'Ivan', status: 'I am a developer', location: {city: 'Yuznoukrainsk', country: 'Ukraine'}},
-      {id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVqhTJArI2RLSSzlY0X1twqrkjMrScCsRWwQ&usqp=CAU',
-        followed: true, fullName: 'Andrew', status: 'Looking for a job', location: {city: 'Kiev', country: 'Ukraine'}},
-      {id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVqhTJArI2RLSSzlY0X1twqrkjMrScCsRWwQ&usqp=CAU',
-        followed: false, fullName: 'Nikita', status: 'I am crazy', location: {city: 'Moskow', country: 'Russia'}},
-      {id: 4, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVqhTJArI2RLSSzlY0X1twqrkjMrScCsRWwQ&usqp=CAU',
-        followed: true, fullName: 'Sasha', status: 'Love cats !', location: {city: 'Minsk', country: 'Belarus'}},
-      {id: 5, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVqhTJArI2RLSSzlY0X1twqrkjMrScCsRWwQ&usqp=CAU',
-        followed: false, fullName: 'Julia', status: 'Learning JS', location: {city: 'New-York', country: 'USA'}}
-    ])
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        props.setUsers(response.data.items)
+      })
   }
 
   return(
@@ -48,12 +44,12 @@ const Users =(props)=>{
       {
         props.users.map( user => 
           <div key ={user.id}>
-            <Img src={user.photoUrl} />
+            <Img src={user.photos.small != null ? user.photos.small : userPhoto} />
             <div>
-              <div>{user.fullName}</div>
+              <div>{user.name}</div>
               <div>Status: {user.status}</div>
-              <div>Country: {user.location.country}</div>
-              <div>City: {user.location.city}</div>
+              <div>Country: {'user.location.country'}</div>
+              <div>City: {'user.location.city'}</div>
               {user.followed 
                 ? <Button onClick={()=>{props.unfollow(user.id)}}>Unfollow</Button>
                 : <Button onClick={()=>{props.follow(user.id)}}>Follow</Button>}
