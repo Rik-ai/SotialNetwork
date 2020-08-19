82f89281e3dadce752eaa7ae62185741008ce0d6
Ivan <ivan.lakhanin@gmail.com>
2020-08-19 11:21:05 +0300

    Comment for preloader

--- src/app/components/Users/UsersContainer.js
+++ src/app/components/Users/UsersContainer.js
@@ -37,7 +37,8 @@ class UsersAPIContainer extends React.Component {
 
   render() { 
     return <>
-    {this.props.isFetching ? <Preloader/> : null}
+    {/* прелоадер не крутится почему-то */}
+    {this.props.isFetching ? <Preloader/> : null} 
      
     <Users
       totalUsersCount={this.props.totalUsersCount}
